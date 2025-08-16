337e718c0619b3e3c5f6ebeda5fa73c951a2e5b5
Graham Place <grahamgplace@gmail.com>
2025-08-15 20:21:32 -0700

    unstable_noStore to fix cached getLatestUpdatedDatetime bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { getLatestUpdatedDatetime } from "@/store/metadata";
 import "server-only";
+import { unstable_noStore } from "next/cache";
 import Map from "@/components/Map";
 import { envobj, string } from "envobj";
 
@@ -11,6 +12,7 @@ const env = envobj(
 );
 
 export default async function Page() {
+  unstable_noStore();
   const dataAsOf = await getLatestUpdatedDatetime();
   return (
     <div className="min-h-screen w-[100vw] flex items-center justify-center">
