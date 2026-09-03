9d9b488b42ccd965dd871851bb8fb4a1d52e083c
Graham Place <grahamgplace@gmail.com>
2026-09-03 09:20:49 -0700

    Re-enable inngest jobs

--- src/inngest/functions/index.ts
+++ src/inngest/functions/index.ts
@@ -1,1 +1,11 @@
-export const functions = [];
+import { updateServiceRequests } from "./update-service-requests";
+import {
+  processBatchFunction,
+  weeklyBackfillScheduler,
+} from "./backfill-query-tags";
+
+export const functions = [
+  updateServiceRequests,
+  processBatchFunction,
+  weeklyBackfillScheduler,
+];
